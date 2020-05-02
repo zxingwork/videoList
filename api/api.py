b068719455a706e34b5c1d42cd260d0b749410a0
zxingwork <zxingwork@163.com>
2020-05-02 20:54:44 +0800

    Initial commit

---
+++ api/api.py
@@ -0,0 +1,34 @@
+import flask
+import json
+import os, csv, time
+import random
+import struct
+import modbus_tk.defines as md
+import modbus_tk.modbus_tcp as mt
+from flask_cors import *
+server = flask.Flask(__name__)
+CORS(server,supports_credentials=True)
+
+
+@server.route('/videolist', methods=['get'])
+def getVideoList():
+    filepath = '/Volumes/zxingspace/video'
+    videolist = get_file_list(filepath)
+    videodict = dict(zip(range(len(videolist)), videolist))
+    return videodict
+
+
+def get_file_list(file_path):
+    dir_list = os.listdir(file_path)
+    if not dir_list:
+        return
+    else:
+        # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
+        # os.path.getmtime() 函数是获取文件最后修改时间
+        # os.path.getctime() 函数是获取文件最后创建时间
+        dir_list = sorted(dir_list, key=lambda x: os.path.getmtime(os.path.join(file_path, x)), reverse=True)
+        # print(dir_list)
+        return dir_list
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0',port='9527',debug=True)
---
+++ js/common.js
@@ -0,0 +1,50 @@
+
+function videolist() {
+    let numPerPage = 20;
+    var video;
+    var ip_addr = document.location.hostname
+    $.get("http://"+ip_addr+":9527/videolist", function (result) {
+
+        totalCount = Object.keys(result).length;
+        document.getElementById("total").innerText = totalCount;
+        resultDict = result;
+        pageitemNum = Math.round(totalCount/numPerPage);
+        console.log(pageitemNum)
+        for (var i=1;i<=pageitemNum;i++){
+            var x = document.createElement("li");
+            x.setAttribute("title", i);
+            x.setAttribute("class", "be-page-item");
+            x.setAttribute("onclick","clickbepageritem(event)")
+            x.innerHTML=i;
+            document.getElementById("be-pager").appendChild(x);
+        }
+        //////////
+        return result;
+    })
+}
+
+function Pagelist(pagetitle) {
+    numPerPage = 20;
+    var resultList = new Array();
+    for (var i=(pagetitle-1)*20;i<pagetitle*20;i++){
+        resultList[i]=resultDict[i]
+    }
+    console.log(resultList)
+    return resultList
+}
+
+function clickbepageritem(event) {
+    pagetitle = event.currentTarget;
+    var list = new Array();
+    list = Pagelist(pagetitle.title);
+    console.log(pagetitle.title)
+    document.getElementById("showvideo").innerHTML="";
+    for (key in list){
+        if (list[key] != undefined){
+            var x = document.createElement("video");
+            x.setAttribute("controls", "controls");
+            x.setAttribute("src", "/video/"+list[key]);
+            document.getElementById("showvideo").appendChild(x);
+        }
+    }
+}
